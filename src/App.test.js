1552d553dd5caac3652ae9409a5c1303e250b258
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-02-11 23:24:36 -0500

    snapshot-tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-// import renderer from 'react-test-renderer'
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import renderer from 'react-test-renderer'
 
-// test('renders a snapshot', () => {
-//   const tree = renderer.create(<App/>).toJSON()
-//   expect(tree).toMatchSnapshot()
-// })
+test('renders a snapshot', () => {
+  const tree = renderer.create(<App/>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
